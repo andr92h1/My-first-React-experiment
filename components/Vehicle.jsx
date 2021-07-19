6ac512ea269625c86c4493e641b2ebd421b707e7
Andreea Teodora Panut <atp@greenmobility.com>
2021-07-19 14:35:01 +0200

    VehicleComponent

---
+++ components/Vehicle.jsx
@@ -0,0 +1,18 @@
+import Class from "./vehicle.module.css";
+
+export const VehicleInfo = () => {
+  return (
+    <div class="Vehicle1" className={Class.vehicle} id="vehicle-component">
+      <img
+        src="https://play-lh.googleusercontent.com/Lj-hSgz9aLpL4bmDByJAhH-HbvjLhS0DU8E-v_tHeK647mQRtlW5YwdBVKNCiGLO-bE=s360-rw"
+        alt="Vehicle"
+        width="42"
+        height="42"
+      />
+      BE 2033 12
+      <p>Batery percentage:</p>
+      <p>Location:</p>
+      <p>Car Type:</p>
+    </div>
+  );
+};
---
+++ components/Vehicle.stories.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+import { VehicleInfo } from "./Vehicle";
+
+export const AndreeaVehicleComponent = () => {
+  return <VehicleInfo></VehicleInfo>;
+};
+
+export const AndreeaVehicleComponent = () => {
+  return <VehicleInfo></VehicleInfo>;
+};
+
+export default {
+  title: "Vehicle",
+};
